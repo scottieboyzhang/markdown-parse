06626348e00b0932a7ddca480c75ee7b5fea6c70
scottieboyzhang <97482103+scottieboyzhang@users.noreply.github.com>
2022-01-28 14:51:23 -0800

    create test file

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,9 +1,22 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+import java.util.List;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.io.IOException;
 
 public class MarkdownParseTest {
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
+    }
+
+    @Test
+    public void testGetLinks()throws IOException{
+       
+        Path fileName = Path.of("test-file2.md");
+        String contents = Files.readString(fileName);
+        assertEquals("https://something.com","some-page.html", MarkdownParse.getLinks(contents));     
+   
     }
 }
