6f2faa039303c45582157fd9c5b63fb8c61879e0
scottieboyzhang <97482103+scottieboyzhang@users.noreply.github.com>
2022-02-10 11:45:22 -0800

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -5,7 +5,7 @@ import java.nio.file.Path;
 import java.util.ArrayList;
 
 public class MarkdownParse {
-    public static ArrayList<String> getLinks(String markdown) throws IOException {
+    public static ArrayList<String> getLinks(String markdown){
         ArrayList<String> toReturn = new ArrayList<>();
         // find the next [, then find the ], then find the (, then take up to
         // the next )
